070008468d0b73e0d5153c1a55b095f90bea07cf
YebasMardo <yebasmardo@gmail.com>
2025-11-22 03:47:14 +0100

    22/11/25 03:47

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+    const [inputValue, setInputValue] = useState("");
+    function handleInput(e) {
+        setInputValue(e.target.value);
+    }
+
+    function handleBlur() {
+        if (!inputValue.includes("@")) {
+            alert(
+                "Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥"
+            );
+        }
+    }
+    return (
+        <footer className="lmj-footer">
+            <div className="lmj-footer-elem">
+                Pour les passionné·e·s de plantes 🌿🌱🌵
+            </div>
+            <div className="lmj-footer-elem">
+                Laissez-nous votre mail :{" "}
+                <input
+                    placeholder="Entrer votre email"
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInput}
+                    onBlur={handleBlur}
+                />
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
